3d5025ebf3ac1998d99e69adb0e6c22ee8a1ddf6
CyprianESPI <cyprianespinoza@gmail.com>
2024-04-21 15:33:18 +0200

    Use M2 in ingredient input

--- src/app/ingredient-input/ingredient-input.component.ts
+++ src/app/ingredient-input/ingredient-input.component.ts
@@ -1,14 +1,65 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component, input } from '@angular/core';
 import { Ingredient } from '../models/ingredient';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { FormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @Component({
   selector: 'app-ingredient-input',
   standalone: true,
-  imports: [CommonModule],
-  template: `<p>{{ ingredient().name }}</p>
-    <p>{{ ingredient().quantity }}</p>
-    <p>{{ ingredient().unit }}</p>`,
+  imports: [
+    CommonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    MatButtonModule,
+    MatIconModule,
+  ],
+  template: `<mat-form-field>
+      <mat-label>Name</mat-label>
+      <input matInput type="text" [(ngModel)]="ingredient().name" />
+      @if (ingredient().name) {
+      <button
+        matSuffix
+        mat-icon-button
+        aria-label="Clear"
+        (click)="ingredient().name = ''"
+      >
+        <mat-icon>close</mat-icon>
+      </button>
+      }
+    </mat-form-field>
+    <mat-form-field>
+      <mat-label>Quantity</mat-label>
+      <input matInput type="number" [(ngModel)]="ingredient().quantity" />
+      @if (ingredient().quantity) {
+      <button
+        matSuffix
+        mat-icon-button
+        aria-label="Clear"
+        (click)="ingredient().quantity = 0"
+      >
+        <mat-icon>close</mat-icon>
+      </button>
+      }
+    </mat-form-field>
+    <mat-form-field>
+      <mat-label>Unit</mat-label>
+      <input matInput type="text" [(ngModel)]="ingredient().unit" />
+      @if (ingredient().unit) {
+      <button
+        matSuffix
+        mat-icon-button
+        aria-label="Clear"
+        (click)="ingredient().unit = ''"
+      >
+        <mat-icon>close</mat-icon>
+      </button>
+      }
+    </mat-form-field>`,
   styleUrl: './ingredient-input.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
