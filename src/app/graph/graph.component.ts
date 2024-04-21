b422d81fc00fad5b1dd9d08b54882c6fdea3cc45
CyprianESPI <cyprianespinoza@gmail.com>
2024-04-21 17:05:49 +0200

    Drawing graph in Canvas wip

--- src/app/graph/graph.component.ts
+++ src/app/graph/graph.component.ts
@@ -1,12 +1,50 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {
+  AfterViewInit,
+  ChangeDetectionStrategy,
+  Component,
+  ElementRef,
+  ViewChild,
+} from '@angular/core';
 
 @Component({
   selector: 'app-graph',
   standalone: true,
   imports: [CommonModule],
-  template: `<p>graph works!</p>`,
+  template: `<canvas #graph></canvas>`,
   styleUrl: './graph.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class GraphComponent {}
+export class GraphComponent implements AfterViewInit {
+  @ViewChild('graph') graph!: ElementRef<HTMLCanvasElement>;
+  ctx: CanvasRenderingContext2D | null = null;
+
+  ngAfterViewInit(): void {
+    this.ctx = this.graph.nativeElement.getContext('2d');
+    console.log('ngAfterViewInit', this.ctx);
+
+    setInterval(() => {
+      this.draw();
+    }, 1000 + 0 * 42);
+  }
+  /* important! for alignment, you should make things
+   * relative to the canvas' current width/height.
+   */
+  private draw() {
+    const ctx = this.ctx;
+    if (!ctx) {
+      return;
+    }
+    ctx.canvas.width = window.innerWidth;
+    ctx.canvas.height = window.innerHeight;
+
+    // Clear
+    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+
+    //...drawing code...
+    console.log('draw', ctx);
+    ctx.fillRect(25, 25, 100, 100);
+    ctx.clearRect(45, 45, 60, 60);
+    ctx.strokeRect(50, 50, 50, 50);
+  }
+}
