64172ccdaefe000ebf0686125518c94f39e9988b
CyprianESPI <cyprianespinoza@gmail.com>
2024-05-04 16:09:11 +0200

    Update table with mix, part 1

--- src/app/table/table.component.ts
+++ src/app/table/table.component.ts
@@ -1,7 +1,13 @@
 import { CommonModule } from '@angular/common';
-import { Component, Input, OnInit } from '@angular/core';
-import { Ingredient } from '../models/ingredient';
+import { Component, input } from '@angular/core';
 import { MatTableModule } from '@angular/material/table';
+import { Db } from '../models/db';
+import { Ingredient } from '../models/ingredient';
+
+interface DataPoint {
+  id: number;
+  ingredients: Ingredient[];
+}
 
 @Component({
   selector: 'app-table',
@@ -9,33 +15,82 @@ import { MatTableModule } from '@angular/material/table';
   imports: [CommonModule, MatTableModule],
   template: `<table
     mat-table
-    [dataSource]="ingredients"
+    [dataSource]="getDataPoints()"
     class="mat-elevation-z8"
   >
     <!-- 1st Column -->
-    <ng-container matColumnDef="1">
-      <th mat-header-cell *matHeaderCellDef>Name</th>
-      <td mat-cell *matCellDef="let element">{{ element.name }}</td>
+    <ng-container [matColumnDef]="getDisplayedColumns()[0]">
+      <th mat-header-cell *matHeaderCellDef>N°</th>
+      <td mat-cell *matCellDef="let element">{{ element.id }}</td>
     </ng-container>
 
-    <ng-container matColumnDef="2">
-      <th mat-header-cell *matHeaderCellDef>Quantity</th>
+    <ng-container [matColumnDef]="getDisplayedColumns()[1]">
+      <th mat-header-cell *matHeaderCellDef>
+        {{ data().ingredients[0].name }}
+      </th>
+      <td mat-cell *matCellDef="let element">
+        {{ element.quantity + ' ' + element.unit }}
+      </td>
+    </ng-container>
+
+    <ng-container [matColumnDef]="getDisplayedColumns()[2]">
+      <th mat-header-cell *matHeaderCellDef>
+        {{ data().ingredients[1].name }}
+      </th>
+      <td mat-cell *matCellDef="let element">
+        {{ element.quantity + ' ' + element.unit }}
+      </td>
+    </ng-container>
+
+    <ng-container [matColumnDef]="getDisplayedColumns()[3]">
+      <th mat-header-cell *matHeaderCellDef>
+        {{ data().ingredients[2].name }}
+      </th>
       <td mat-cell *matCellDef="let element">
         {{ element.quantity + ' ' + element.unit }}
       </td>
     </ng-container>
 
     <!-- Table generation  -->
-    <tr mat-header-row *matHeaderRowDef="displayedColumns"></tr>
-    <tr mat-row *matRowDef="let row; columns: displayedColumns"></tr>
+    <tr mat-header-row *matHeaderRowDef="getDisplayedColumns()"></tr>
+    <tr mat-row *matRowDef="let row; columns: getDisplayedColumns()"></tr>
   </table>`,
   styleUrl: './table.component.scss',
 })
-export class TableComponent implements OnInit {
-  ngOnInit(): void {
-    console.log(this.ingredients);
-  }
-  @Input()
-  ingredients: Ingredient[] = [];
+export class TableComponent {
+  data = input.required<Db>();
   displayedColumns: string[] = ['1', '2'];
+
+  getDisplayedColumns(): string[] {
+    if (!this.data) return ['Id'];
+
+    const res: string[] = ['Id'];
+    const ingredients = this.data().ingredients;
+    ingredients.forEach((ingredient) => {
+      res.push(ingredient.name);
+    });
+
+    return res;
+  }
+
+  getDataPoints(): DataPoint[] {
+    if (!this.data) return [];
+
+    const steps = this.data().steps;
+    const ingredients = this.data().ingredients;
+    const res: DataPoint[] = [];
+
+    for (let i = 0; i < steps + 1; i++) {
+      for (let j = 0; j < i + 1; j++) {
+        const prevId = res?.at(-1)?.id ?? 0;
+        const datapoint: DataPoint = {
+          id: prevId + 1,
+          ingredients: ingredients,
+        };
+        res.push(datapoint);
+      }
+    }
+
+    return res;
+  }
 }
